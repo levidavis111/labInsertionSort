cb985dbbbb1534a60433679110009371ce88fe62
levi davis <levidavis@pursuit.org>
2019-10-16 09:56:20 -0400

    First commint

--- Pursuit-Core-Insertion-Sort-Lab/InsertionSortQuestions.swift
+++ Pursuit-Core-Insertion-Sort-Lab/InsertionSortQuestions.swift
@@ -2,8 +2,20 @@ import Foundation
 
 // Question One
 // Implement insertion sort that accepts a closure about how to sort elements
-func insertionSorted<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
-    return arr
+
+func insert<T>(_ newElement: T, into sortedArr: [T], by isSorted: (T,T) -> Bool) -> [T] {
+    var sortedArr = sortedArr
+    
+    for (index, element) in sortedArr.enumerated() {
+        
+        if isSorted(newElement, element) {
+            sortedArr.insert(newElement, at: index)
+            return sortedArr
+        }
+    }
+    sortedArr.append(newElement)
+    return sortedArr
+    
 }
 
 // Question Two
@@ -16,6 +28,17 @@ func insertionSortWithoutMovingNegatives<T: Comparable>(arr: [T], by isSorted: (
 // Question Three
 // Implement insertion sort in place
 func insertionSort<T: Comparable>(arr: inout [T], by isSorted: (T, T) -> Bool) -> [T] {
+    
+        for indexOne in 1..<arr.count {
+            
+            var indexTwo = indexOne
+            while indexTwo > 0 && !isSorted(arr[indexTwo - 1], arr[indexTwo]) {
+                arr.swapAt(indexTwo - 1, indexTwo)
+                indexTwo -= 1
+               
+            }
+        }
+    
     return arr
 }
 
